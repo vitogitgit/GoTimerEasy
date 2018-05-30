44121595adf5884dad000257fa794904697886f8
Vito <mishmshi@hopenglish.com>
2018-05-31 00:50:57 +0800

    [What] Take out the constant

---
+++ src/components/Constant.js
@@ -0,0 +1,5 @@
+export const INITIAL_TIME = 5;
+// export const INITIAL_TIME = 300;
+export const RUNNER_BORDER_COLOR = '#e15f41';
+export const STOPPER_BORDER_COLOR = '#d1ccc070';
+export const TOUCH_AREA_UNDERLAY_COLOR = 'rgba(0, 0, 0, 0.05)';
