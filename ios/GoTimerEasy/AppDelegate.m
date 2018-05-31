609fb830c38587e85856a748d4889d9313e5c58b
Vito <mishmshi@hopenglish.com>
2018-05-31 16:17:03 +0800

    [What] Add js code location with jsbundle.

--- ios/GoTimerEasy/AppDelegate.m
+++ ios/GoTimerEasy/AppDelegate.m
@@ -17,6 +17,7 @@
   NSURL *jsCodeLocation;
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+//  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"bundle/index" withExtension:@"jsbundle"];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"GoTimerEasy"
