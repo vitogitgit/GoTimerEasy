deffb6a6f249f2cb03f614dfb777dd56815c2faa
Vito <mishmshi@hopenglish.com>
2018-06-10 00:05:40 +0800

    Change folder structure

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { AppRegistry, YellowBox } from 'react-native';
-import App from './src/components/Navigator';
+import App from './src/Navigator';
 
 YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
 AppRegistry.registerComponent('GoTimerEasy', () => App);
