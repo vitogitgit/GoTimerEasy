482fa7190b18684a8c813aedc6d1f294e904ede9
Vito <mishmshi@hopenglish.com>
2020-06-22 19:01:28 +0800

    [What] remove all native

--- oldRN/index.js
+++
@@ -1,9 +0,0 @@
-import { AppRegistry, YellowBox } from 'react-native';
-import App from './src/Navigator';
-
-YellowBox.ignoreWarnings([
-  'Warning: isMounted(...) is deprecated',
-  'Module RCTImageLoader',
-  'Class RCTCxxModule',
-]);
-AppRegistry.registerComponent('GoTimerEasy', () => App);
